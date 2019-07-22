5580b32a501a498eedea2a0638cea4908a8abd36
David W. Rankin Jr <david@bohemians.us>
2019-07-21 23:17:57 -0400

    I rebuilt the SwiftAbstractLogger project fork

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "SwiftSSDP", targets: ["SwiftSSDP"])
     ],
     dependencies: [
-        .package(url: "https://github.com/davidrankin2/SwiftAbstractLogger.git", from: "0.4.0"),
+        .package(url: "https://github.com/davidrankin2/SwiftAbstractLogger.git", from: "0.3.1"),
         .package(url: "https://github.com/davidrankin2/CocoaAsyncSocket.git", from: "7.6.4"),
     ],
     targets: [
